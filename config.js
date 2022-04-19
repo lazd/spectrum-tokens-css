1ce6aae2587571b4634c6cc4911377f2c8176740
Garth Braithwaite <garthdb@gmail.com>
2022-04-19 03:58:51 -0600

    initial commit

---
+++ config.js
@@ -0,0 +1,63 @@
+const StyleDictionary = require("style-dictionary");
+const CSSSetsFormatter = require("style-dictionary-sets").CSSSetsFormatter;
+const NameKebabTransfom = require("style-dictionary-sets").NameKebabTransfom;
+const AttributeSetsTransform =
+  require("style-dictionary-sets").AttributeSetsTransform;
+
+StyleDictionary.registerTransform(NameKebabTransfom);
+StyleDictionary.registerTransform(AttributeSetsTransform);
+StyleDictionary.registerFormat(CSSSetsFormatter);
+
+const generateFileConfig = (subSystemName, setName) => {
+  return {
+    destination: `${subSystemName}/${setName}-vars.css`,
+    format: CSSSetsFormatter.name,
+    filter: (token) => {
+      return (
+        typeof token.attributes === "object" &&
+        !Array.isArray(token.attributes) &&
+        token.attributes !== null &&
+        "sets" in token.attributes &&
+        token.attributes.sets.includes(setName)
+      );
+    },
+    options: {
+      showFileHeader: false,
+      outputReferences: true,
+      sets: [setName, subSystemName],
+    },
+  }
+}
+
+module.exports = {
+  source: ["node_modules/@adobe/spectrum-tokens/src/**/*.json"],
+  platforms: {
+    CSS: {
+      buildPath: "dist/css/",
+      transforms: [AttributeSetsTransform.name, NameKebabTransfom.name],
+      files: [
+        {
+          destination: "default-vars.css",
+          format: CSSSetsFormatter.name,
+          filter: (token) => {
+            return !("sets" in token.attributes);
+          },
+          options: {
+            showFileHeader: false,
+            outputReferences: true,
+          },
+        },
+        generateFileConfig("spectrum","desktop"),
+        generateFileConfig("spectrum","mobile"),
+        generateFileConfig("spectrum","light"),
+        generateFileConfig("spectrum","dark"),
+        generateFileConfig("spectrum","darkest"),
+        generateFileConfig("express","desktop"),
+        generateFileConfig("express","mobile"),
+        generateFileConfig("express","light"),
+        generateFileConfig("express","dark"),
+        generateFileConfig("express","darkest"),
+      ],
+    },
+  },
+};
